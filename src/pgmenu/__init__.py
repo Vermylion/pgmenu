ad972dd83a2e92dbde6ea4704384ba3bf575867f
Vermylion <redninjagflameboy@gmail.com>
2023-03-05 18:52:14 +0100

    pgmenu 1.5

---
+++ src/pgmenu/__init__.py
@@ -0,0 +1,27 @@
+# Import color variables
+from .pgmenu import mainColor
+from .pgmenu import bgColor
+from .pgmenu import fgColor
+from .pgmenu import widgetFgColor
+from .pgmenu import widgetBgColor
+from .pgmenu import textFgColor
+from .pgmenu import textBgColor
+from .pgmenu import widgetOutlineColor
+from .pgmenu import widgetOutlineWidth
+from .pgmenu import widgetBorderWidth
+from .pgmenu import accentColor
+
+# Import Classes
+from .pgmenu import Theme
+from .pgmenu import Menu
+from .pgmenu import Text
+from .pgmenu import Image
+from .pgmenu import Frame
+from .pgmenu import Button
+from .pgmenu import Checkbox
+from .pgmenu import Textbox
+
+# Import individual functions
+from .pgmenu import update
+from .pgmenu import draw
+from .pgmenu import requestCursor
